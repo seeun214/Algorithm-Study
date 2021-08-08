8cb55b9419dab0293402b0c6f9f6dfb59749909a
pse <seeunjin22@naver.com>
2021-08-09 01:21:19 +0900

    Update 모의고사.java
    2021-08-09

--- Programmers/Level1/모의고사.java
+++ Programmers/Level1/모의고사.java
@@ -8,7 +8,7 @@ public class 모의고사 {
 
 	public static void main(String[] args) {
 		Solution s = new Solution();
-		int answers[] = {1, 3, 2, 4, 2};
+		int answers[] = {1, 2, 3, 4, 5};
 		s.solution(answers);
 	}
 }
