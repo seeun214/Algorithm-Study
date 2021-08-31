6e85255289702c67a5439ee639d42104773d1982
pse <seeunjin22@naver.com>
2021-08-31 23:05:45 +0900

    20210831

--- Baekjoon/Binary_10829.java
+++ Baekjoon/Binary_10829.java
@@ -5,23 +5,6 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class Binary_10829 {
-
-//	public static void main(String[] args) {
-//		Scanner scan = new Scanner(System.in);
-//		
-//		long N = scan.nextLong();
-//		int mod = 0;
-//		Stack<Integer> answer = new Stack<>();
-//		
-//		for(long i = N; i > 0; i /= 2) {
-//			answer.add((int)(i % 2));
-//		}
-//		
-//		while(!answer.isEmpty()) {
-//			System.out.print(answer.pop());
-//		}
-//	}
-	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		String temp = br.readLine();
