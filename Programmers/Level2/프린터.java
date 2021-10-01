72854bc5292d492ceee46205c9b1c2f1d0bd1e3f
pse <seeunjin22@naver.com>
2021-10-01 23:44:42 +0900

    20211001

---
+++ Programmers/Level2/프린터.java
@@ -0,0 +1,38 @@
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class 프린터 {
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		int[] priorities = {1, 1, 9, 1, 1, 1};
+		int location = 0;
+		System.out.println(s.solution(priorities, location));
+		
+	}
+
+}
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        int answer = 1;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for(int p : priorities) {
+        	pq.offer(p);
+        }
+       
+        while(!pq.isEmpty()){
+            for(int i = 0; i < priorities.length; i++){
+                if(priorities[i] == (int)pq.peek()) {
+                    if(i == location){
+                        return answer;
+                    }
+                    pq.poll();
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
