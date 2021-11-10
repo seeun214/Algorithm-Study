63cd36493e329ee5a4cbc46bf4a87252f57f2e1a
seeun214 <seeunjin22@naver.com>
2021-11-11 07:03:57 +0900

    20211111

--- Baekjoon/유기농배추_1012.java
+++ Baekjoon/유기농배추_1012.java
@@ -9,45 +9,32 @@ public class 유기농배추_1012 {
 	static int[][] cabbage;
 	static boolean[][] visit;
 	static int count;
-	static int[] dx = { 0, -1, 0, 1 };
-	static int[] dy = { 1, 0, -1, 0 };
-
-	static void dfs(int x, int y) {
-		visit[x][y] = true;
-
-		for (int i = 0; i < 4; i++) {
-			int cx = x + dx[i];
-			int cy = y + dy[i];
-
-			if (cx >= 0 && cy >= 0 && cx < M && cy < N) {
-				if (!visit[cx][cy] && cabbage[cx][cy] == 1) {
-					dfs(cx, cy);
-				}
-			}
-
-		}
-
-	}
+	static int[] dx = { -1, 1, 0, 0 };
+	static int[] dy = { 0, 0, -1, 1 };
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int tc = Integer.parseInt(br.readLine());
-
-		for (int i = 0; i < tc; i++) {
+		//테스트 케이스의 개수 T
+		int T = Integer.parseInt(br.readLine());
+		
+		for (int i = 0; i < T; i++) {  
 			count = 0;
 			StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+			//배추밭 가로길이
 			M = Integer.parseInt(st.nextToken());
+			//배추밭 세로길이
 			N = Integer.parseInt(st.nextToken());
 			cabbage = new int[M][N];
 			visit = new boolean[M][N];
-
+			
+			//배추가 심어져 있는 위치의 개수
 			K = Integer.parseInt(st.nextToken());
 			for (int j = 0; j < K; j++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				int p1 = Integer.parseInt(st.nextToken());
-				int p2 = Integer.parseInt(st.nextToken());
-				cabbage[p1][p2] = 1;
+				int k1 = Integer.parseInt(st.nextToken());
+				int k2 = Integer.parseInt(st.nextToken());
+				cabbage[k1][k2] = 1;
 			}
 
 			for (int x = 0; x < M; x++) {
@@ -58,10 +45,22 @@ public class 유기농배추_1012 {
 					}
 				}
 			}
-
 			System.out.println(count);
 		}
-
 	}
+	
+	static void dfs(int x, int y) {
+		visit[x][y] = true;
+		//상하좌우 배추 제거?
+		for (int i = 0; i < 4; i++) {
+			int cx = x + dx[i];
+			int cy = y + dy[i];
 
+			if (cx >= 0 && cy >= 0 && cx < M && cy < N) {
+				if (!visit[cx][cy] && cabbage[cx][cy] == 1) {
+					dfs(cx, cy);
+				}
+			}
+		}
+	}
 }
