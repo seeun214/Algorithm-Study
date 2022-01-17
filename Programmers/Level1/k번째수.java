764cd8c922c835acb72d2c5ce1d0a2693a5700a1
seeun214 <seeunjin22@naver.com>
2022-01-17 18:01:16 +0900

    20220117

---
+++ Programmers/Level1/k번째수.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        
+        for(int i = 0; i < commands.length ; i++){
+            int[] newArray = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+            Arrays.sort(newArray);
+            System.out.println(Arrays.toString(newArray));
+            answer[i] = newArray[commands[i][2] - 1];
+            
+        }
+   
+        return answer;
+    }
+}
